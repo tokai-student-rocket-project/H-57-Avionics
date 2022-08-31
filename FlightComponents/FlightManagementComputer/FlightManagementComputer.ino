8c5cd92ecd3cea176c7c682b3f4aa07f2cae8fe3
waiwai2525 <waiwai.cps@gmail.com>
2022-08-31 16:14:48 +0900

    グローバル変数を名前空間の中に入れた

--- FlightComponents/FlightManagementComputer/FlightManagementComputer.ino
+++ FlightComponents/FlightManagementComputer/FlightManagementComputer.ino
@@ -2,38 +2,76 @@
 #include <MsgPacketizer.h>
 #include "MovingAverage.h"
 
-MultiUART DEBUG(Serial);
-MultiUART FDC(2, 3);
+enum FlightMode {
+  standby,
+  descent
+};
 
-MovingAverage PressureAverage;
-float PressureAverageOrigin;
-int EdgeCount = 0;
+namespace serial {
+  const MultiUART debug(Serial);
+  const MultiUART flight_data_computer(2, 3);
 
-void setup()
-{
-  Serial.begin(9600);
-  FDC.begin(9600);
-
-  MsgPacketizer::subscribe(FDC, 0x00, [](float altitude)
-                           {
-  PressureAverage.addData(altitude);
-  float average = PressureAverage.getAverage();
-
-  if (average < PressureAverageOrigin){
-    EdgeCount++;
-  }
-  else{
-    EdgeCount = 0;
-  }
-
-  PressureAverageOrigin = average;
-
-  if (EdgeCount >= 10){
-    DEBUG.println("Apogee");
-  } });
+  // データ種別、FlightDataComputerのREADME参照
+  constexpr int ALTITUDE_LABEL = 0x00;
 }
 
-void loop()
-{
+namespace flightdata {
+  FlightMode flight_mode = FlightMode::standby;
+  float altitude;
+}
+
+namespace descentdetection {
+  const MovingAverage pressure_average(0.3);
+  float altitude_average_old;
+  int descent_count;
+}
+
+void setup() {
+  serial::debug.begin(9600);
+  serial::flight_data_computer.begin(9600);
+
+  pinMode(8, OUTPUT);
+
+  MsgPacketizer::subscribe(
+    serial::flight_data_computer,
+    serial::ALTITUDE_LABEL,
+    [](float altitude) {
+      updateAltitude(altitude);
+    });
+}
+
+void loop() {
   MsgPacketizer::parse();
+}
+
+void updateAltitude(float altitude) {
+  flightdata::altitude = altitude;
+
+  if (flightdata::flight_mode == FlightMode::standby && isDescending()) {
+    flightdata::flight_mode = FlightMode::descent;
+    digitalWrite(8, HIGH);
+  }
+}
+
+bool isDescending() {
+  float average = descentdetection::pressure_average.getAverage(flightdata::altitude);
+
+  // 高度が連続で減少した回数をdescent_countに加算
+  if (average < descentdetection::altitude_average_old) {
+    ++descentdetection::descent_count;
+  }
+  else {
+    descentdetection::descent_count = 0;
+  }
+
+  descentdetection::altitude_average_old = average;
+
+  serial::debug.print(flightdata::altitude);
+  serial::debug.print("\t");
+  serial::debug.print(average);
+  serial::debug.print("\t");
+  serial::debug.println(descentdetection::descent_count);
+
+  // 10回以上連続で減少したら降下中と判断する
+  return descentdetection::descent_count >= 10;
 }
