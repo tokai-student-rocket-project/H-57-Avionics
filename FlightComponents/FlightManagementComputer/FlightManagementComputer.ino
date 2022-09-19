0e664664e5b07ed8dc945317c0196c6ea5d8b88b
waiwai2525 <waiwai.cps@gmail.com>
2022-09-19 23:58:24 +0900

    フライトモードの処理を分離

--- FlightComponents/FlightManagementComputer/FlightManagementComputer.ino
+++ FlightComponents/FlightManagementComputer/FlightManagementComputer.ino
@@ -13,40 +13,40 @@ enum class FlightMode {
 };
 
 namespace device {
-  // 気圧センサ
-  // https://strawberry-linux.com/support/12122/1812122
-  const LPS22HB _pressureSensor(LPS22HB_DEFAULT_ADDRESS);
+// 気圧センサ
+// https://strawberry-linux.com/support/12122/1812122
+const LPS22HB _pressureSensor(LPS22HB_DEFAULT_ADDRESS);
 
-  const FlightPin _flightPin(12);
-  const Shiranui _shiranui3(10);
-  const LED _climbIndicator(9);
-  const LED _descentIndicator(8);
-}
+const FlightPin _flightPin(12);
+const Shiranui _shiranui3(10);
+const LED _climbIndicator(9);
+const LED _descentIndicator(8);
+} // namespace device
 
-namespace flightdata {
-  double _pressure;
-  double _altitude;
-}
+namespace flightData {
+double _pressure;
+double _altitude;
+} // namespace flightData
 
 namespace internal {
-  FlightMode _flightMode;
-  unsigned long _launchTime;
-}
+FlightMode _flightMode;
+unsigned long _launchTime;
+} // namespace internal
 
 namespace detector {
-  // 引数には平滑化の感度を指定する
-  // ブレッドボードを自由落下させるには0.2くらいがちょうどよかった(適当)
-  DescentDetector _descentDetector(0.2);
-}
+// 引数には平滑化の感度を指定する
+// ブレッドボードを自由落下させるには0.2くらいがちょうどよかった(適当)
+DescentDetector _descentDetector(0.2);
+} // namespace detector
 
-namespace separation {
-  // 燃焼中に分離しないために保護する時間を指定
-  constexpr unsigned long SEPARATION_MINIMUM = 5000;
-  // 強制的に分離する時間を指定
-  constexpr unsigned long SEPARATION_MAXIMUM = 15000;
-  // 分離する高度を指定
-  constexpr double SEPARATION_ALTITUDE = -0.5;
-}
+namespace separationConfig {
+// 燃焼中に分離しないために保護する時間を指定
+constexpr unsigned long SEPARATION_MINIMUM = 5000;
+// 強制的に分離する時間を指定
+constexpr unsigned long SEPARATION_MAXIMUM = 15000;
+// 分離する高度を指定
+constexpr double SEPARATION_ALTITUDE = -0.5;
+} // namespace separation
 
 void setup() {
   Wire.begin();
@@ -59,7 +59,7 @@ void setup() {
   device::_climbIndicator.initialize();
   device::_descentIndicator.initialize();
 
-  changeFlightMode(FlightMode::STANDBY);
+  ::changeFlightMode(FlightMode::STANDBY);
 
   // 初期化直後の外れ値を除くために3秒遅らせる（ローパスフィルタが使えればそっち）
   delay(3000);
@@ -67,52 +67,73 @@ void setup() {
 }
 
 void loop() {
-  flightdata::_pressure = device::_pressureSensor.getPressure();
-  flightdata::_altitude = device::_pressureSensor.getAltitude();
-
-  detector::_descentDetector.updateAltitude(flightdata::_altitude);
+  // センサーを読んでフライトデータを更新する
+  flightData::_pressure = device::_pressureSensor.getPressure();
+  flightData::_altitude = device::_pressureSensor.getAltitude();
+  detector::_descentDetector.updateAltitude(flightData::_altitude);
 
   // フライトピン刺したらリセット
-  if (!device::_flightPin.isReleased()) {
-    device::_shiranui3.reset();
-    changeFlightMode(FlightMode::STANDBY);
-  }
+  if (!device::_flightPin.isReleased()) changeFlightMode(FlightMode::STANDBY);
 
-  // バックアップタイマー
-  if (internal::_flightMode == FlightMode::CLIMB || internal::_flightMode == FlightMode::DESCENT) {
-      if (millis() > internal::_launchTime + separation::SEPARATION_MAXIMUM){
-        device::_shiranui3.separate();
-      }
-  }
+  // 強制分離
+  if (canSeparateForce()) changeFlightMode(FlightMode::MAINCHUTE);
 
   switch (internal::_flightMode) {
     case FlightMode::STANDBY:
       // フライトピンが抜けたらCLIMBモードに移行
-      if (device::_flightPin.isReleased()) {
-        changeFlightMode(FlightMode::CLIMB);
-        internal::_launchTime = millis();
-      }
+      if (device::_flightPin.isReleased()) changeFlightMode(FlightMode::CLIMB);
       break;
     case FlightMode::CLIMB:
-      // DescentDetectorが降下を検出したらDESCENTモードに移行
-      if (detector::_descentDetector._isDescending) {
-        changeFlightMode(FlightMode::DESCENT);
-      }
+      // 降下が始まったらDESCENTモードに移行
+      if (detector::_descentDetector._isDescending) changeFlightMode(FlightMode::DESCENT);
       break;
     case FlightMode::DESCENT:
-      // SEPARATE_ALTITUDE以下になれば分離
-      if (flightdata::_altitude <= separation::SEPARATION_ALTITUDE) {
-        if (millis() > internal::_launchTime + separation::SEPARATION_MINIMUM) {
-          device::_shiranui3.separate();
-        }
-      }
+      // 条件が揃えば分離
+      if (canSeparate()) changeFlightMode(FlightMode::MAINCHUTE);
       break;
   }
 }
 
 void changeFlightMode(FlightMode newFlightMode) {
+  // フライトモードに変更がなければ早期リターン
+  if (newFlightMode == internal::_flightMode) return;
+
+  // デバッグ用
+  // フライトモードに応じてLEDを切り替える
   device::_climbIndicator.set(newFlightMode == FlightMode::CLIMB);
   device::_descentIndicator.set(newFlightMode == FlightMode::DESCENT);
 
+  switch (newFlightMode)
+  {
+    case FlightMode::STANDBY:
+      device::_shiranui3.reset();
+      break;
+    case FlightMode::CLIMB:
+      // 上昇開始時間を記録しておく
+      internal::_launchTime = millis();
+      break;
+    case FlightMode::MAINCHUTE:
+      device::_shiranui3.separate();
+      break;
+  }
+
   internal::_flightMode = newFlightMode;
+}
+
+bool canSeparate() {
+  // 飛行時間がSEPARATION_MINIMUMを超えているか
+  bool isPassedSeparationMinimum = millis() > internal::_launchTime + separationConfig::SEPARATION_MINIMUM;
+  // 高度がSEPARATION_ALTITUDE以下か
+  bool isPassedSeparationAltitude = flightData::_altitude <= separationConfig::SEPARATION_ALTITUDE;
+
+  return isPassedSeparationMinimum && isPassedSeparationAltitude;
+}
+
+bool canSeparateForce() {
+  // 上昇中か下降中
+  bool isInFlight = internal::_flightMode == FlightMode::CLIMB || internal::_flightMode == FlightMode::DESCENT;
+  // 飛行時間がSEPARATION_MAXIMUMを超えているか
+  bool isPassedSeparationMaximum = millis() > internal::_launchTime + separationConfig::SEPARATION_MAXIMUM;
+
+  return isInFlight && isPassedSeparationMaximum;
 }
