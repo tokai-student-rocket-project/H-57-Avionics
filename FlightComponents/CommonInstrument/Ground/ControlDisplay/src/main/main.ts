12efc32744fccd709f2f1d43423226642090071e
waiwai2525 <waiwai.cps@gmail.com>
2022-11-22 15:13:22 +0900

    シリアル通信でjsonが来てもフリーズしないように修正

--- FlightComponents/CommonInstrument/Ground/ControlDisplay/src/main/main.ts
+++ FlightComponents/CommonInstrument/Ground/ControlDisplay/src/main/main.ts
@@ -41,37 +41,41 @@ ipcMain.on('open-serialport', (_, serialportPath: string) => {
   serialport = new SerialPort({ path: serialportPath, baudRate: 115200 });
   const parser = serialport.pipe(new ReadlineParser());
   parser.on('data', (data) => {
-    const dataObject = JSON.parse(data);
+    try {
+      const dataObject = JSON.parse(data);
 
-    if (dataObject.t === 's') {
-      store.set('flight-mode', dataObject.m);
-      store.set('flightpin-state', dataObject.f === '1' ? 'OPEN' : 'CLOSE');
-      store.set('shiranui3-state', dataObject.s3 === '1' ? 'ON' : 'OFF');
-      store.set('buzzer-state', dataObject.b === '1' ? 'ON' : 'OFF');
-      mainWindow?.webContents.send('status-recieved');
-    } else if (dataObject.t === 'f') {
-      store.set('flight-time', dataObject.ft);
-      store.set('altitude', dataObject.alt);
-      store.set('acceleration-x', dataObject.ax);
-      store.set('acceleration-y', dataObject.ay);
-      store.set('acceleration-z', dataObject.az);
-      mainWindow?.webContents.send('flight-data-recieved');
-    } else if (dataObject.t === 'c') {
-      store.set('base-pressure', dataObject.p);
-      store.set('burn-time', dataObject.b);
-      store.set('separation-minimum', dataObject.smin);
-      store.set('separation-maximum', dataObject.smax);
-      mainWindow?.webContents.send('config-recieved');
-    } else if (dataObject.t === 'e') {
-      console.log(dataObject.e);
-      mainWindow?.webContents.send(
-        'event-recieved',
-        dataObject.e as string,
-        dataObject.ft as string
-      );
-    } else if (dataObject.t === 'r') {
-      store.set('rssi', dataObject.rssi);
-      mainWindow?.webContents.send('rssi-recieved');
+      if (dataObject.t === 's') {
+        store.set('flight-mode', dataObject.m);
+        store.set('flightpin-state', dataObject.f === '1' ? 'OPEN' : 'CLOSE');
+        store.set('shiranui3-state', dataObject.s3 === '1' ? 'ON' : 'OFF');
+        store.set('buzzer-state', dataObject.b === '1' ? 'ON' : 'OFF');
+        mainWindow?.webContents.send('status-recieved');
+      } else if (dataObject.t === 'f') {
+        store.set('flight-time', dataObject.ft);
+        store.set('altitude', dataObject.alt);
+        store.set('acceleration-x', dataObject.ax);
+        store.set('acceleration-y', dataObject.ay);
+        store.set('acceleration-z', dataObject.az);
+        mainWindow?.webContents.send('flight-data-recieved');
+      } else if (dataObject.t === 'c') {
+        store.set('base-pressure', dataObject.p);
+        store.set('burn-time', dataObject.b);
+        store.set('separation-minimum', dataObject.smin);
+        store.set('separation-maximum', dataObject.smax);
+        mainWindow?.webContents.send('config-recieved');
+      } else if (dataObject.t === 'e') {
+        console.log(dataObject.e);
+        mainWindow?.webContents.send(
+          'event-recieved',
+          dataObject.e as string,
+          dataObject.ft as string
+        );
+      } else if (dataObject.t === 'r') {
+        store.set('rssi', dataObject.rssi);
+        mainWindow?.webContents.send('rssi-recieved');
+      }
+    } catch (error) {
+      console.log(error);
     }
   });
 });
@@ -93,14 +97,20 @@ ipcMain.on('open-serialport-telemeter', (_, serialportPath: string) => {
   });
   const parser = serialportTelemeter.pipe(new ReadlineParser());
   parser.on('data', (data) => {
-    const dataObject = JSON.parse(data);
+    try {
+      const dataObject = JSON.parse(data);
 
-    store.set('latitude', dataObject.lat);
-    store.set('longitude', dataObject.lon);
-    store.set('satellites', dataObject.satellites);
-    store.set('mainservo-degrees', dataObject.mainservoDeg);
-    store.set('supplyservo-degrees', dataObject.supplyservoDeg);
-    mainWindow?.webContents.send('telemetry-recieved');
+      console.log(dataObject);
+
+      store.set('latitude', dataObject.lat);
+      store.set('longitude', dataObject.lon);
+      store.set('satellites', dataObject.satellites);
+      store.set('mainservo-degrees', dataObject.mainservoDeg);
+      store.set('supplyservo-degrees', dataObject.supplyservoDeg);
+      mainWindow?.webContents.send('telemetry-recieved');
+    } catch (error) {
+      console.log(error);
+    }
   });
 });
 
