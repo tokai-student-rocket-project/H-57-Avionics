0db17b92c071ef3293b1d6eb72abac86e9e31679
TsutsumiHiroki <hiroki1815@outlook.jp>
2022-12-05 19:10:23 +0900

    Valveコントロール部の作成

--- FlightComponents/Telemeter/ValveContoroler/ValveContoroler.ino
+++ FlightComponents/Telemeter/ValveContoroler/ValveContoroler.ino
@@ -3,12 +3,16 @@
 VarSpeedServo Mainservo;
 VarSpeedServo Supplyservo;
 
+//Servo Mainservo;
+//Servo Supplyservo;
+
 int MainServoPin = 9;
 int SupplyServoPin = 10;
 float Supplyservo_deg;
 float Mainservo_deg;
 
 volatile int t = 0;
+
 
 void setup()
 {
@@ -29,7 +33,7 @@ void loop()
     Serial.println(Supplyservo_deg);
     Serial.println(Mainservo_deg);
 
-    if(t == 1)
+    if (t == 1)
     {
         t = 0;
         L_Position();
@@ -42,34 +46,66 @@ void loop()
         W_Position();
         delay(10);
     }
-    
 }
 
 void L_SW()
 {
     t = 1;
+    //detachInterrupt(digitalPinToInterrupt(2));
 }
 
 void W_SW()
 {
     t = 2;
+    //detachInterrupt(digitalPinToInterrupt(3));
 }
 
 void L_Position()
 {
     Supplyservo.write(90, 60, true);
+    //Supplyservo.write(90);
     Supplyservo_deg = Supplyservo.read();
     delay(1000);
+
+    /*
+    for (int pos = 0; pos <= 90; pos += 1)
+    {
+        Mainservo.write(pos);
+        delay(20);
+        Mainservo_deg = Mainservo.read();
+    }
+    */
+
+    /*
+    if (Mainservo_deg == 90)
+    {
+        Mainservo.write(90);
+    }
+    */
     Mainservo.write(140, 30, true);
     Mainservo_deg = Mainservo.read();
 }
 
-
 void W_Position()
 {
     Supplyservo.write(0, 60, true);
+    //Supplyservo.write(0);
     Supplyservo_deg = Supplyservo.read();
     delay(1000);
+
+    /*
+    for (int pos = 90; pos >= 0; pos -= 1)
+    {
+        Mainservo.write(pos);
+        delay(20);
+        Mainservo_deg = Mainservo.read();
+    }
+
+    if (Mainservo_deg == 0)
+    {
+        Mainservo.write(0);
+    }
+    */
+
     Mainservo.write(0, 30, true);
-    Mainservo_deg = Mainservo.read();
 }
