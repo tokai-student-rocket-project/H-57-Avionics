a7bc12361f214bb13fe45339bf9ab71bcede4c11
TsutsumiHiroki <hiroki1815@outlook.jp>
2022-12-05 12:04:36 +0900

    Servo速度のコントロール部分を実装

--- FlightComponents/Telemeter/ValveContoroler/ValveContoroler.ino
+++ FlightComponents/Telemeter/ValveContoroler/ValveContoroler.ino
@@ -1,15 +1,75 @@
-# include "VarSpeedServo.h"
+#include "VarSpeedServo.h"
 
 VarSpeedServo Mainservo;
 VarSpeedServo Supplyservo;
 
+int MainServoPin = 9;
+int SupplyServoPin = 10;
+float Supplyservo_deg;
+float Mainservo_deg;
+
+volatile int t = 0;
+
 void setup()
 {
-    Mainservo.attach(9);
-    Supplyservo.attach(8);
+    Serial.begin(9600);
+
+    Mainservo.attach(MainServoPin);
+    Supplyservo.attach(SupplyServoPin);
+
+    pinMode(2, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(2), L_SW, RISING);
+
+    pinMode(3, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(3), W_SW, RISING);
 }
 
 void loop()
 {
+    Serial.println(Supplyservo_deg);
+    Serial.println(Mainservo_deg);
 
+    if(t == 1)
+    {
+        t = 0;
+        L_Position();
+        delay(10);
+    }
+
+    if (t == 2)
+    {
+        t = 0;
+        W_Position();
+        delay(10);
+    }
+    
+}
+
+void L_SW()
+{
+    t = 1;
+}
+
+void W_SW()
+{
+    t = 2;
+}
+
+void L_Position()
+{
+    Supplyservo.write(90, 60, true);
+    Supplyservo_deg = Supplyservo.read();
+    delay(1000);
+    Mainservo.write(140, 30, true);
+    Mainservo_deg = Mainservo.read();
+}
+
+
+void W_Position()
+{
+    Supplyservo.write(0, 60, true);
+    Supplyservo_deg = Supplyservo.read();
+    delay(1000);
+    Mainservo.write(0, 30, true);
+    Mainservo_deg = Mainservo.read();
 }
