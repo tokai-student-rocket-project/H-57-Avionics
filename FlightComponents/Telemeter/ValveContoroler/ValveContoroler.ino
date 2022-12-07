401aa3e6da63e481b9bad7d6ac768dc9c7c94f48
TsutsumiHiroki <hiroki1815@outlook.jp>
2022-12-08 03:36:43 +0900

    コメントの追加

--- FlightComponents/Telemeter/ValveContoroler/ValveContoroler.ino
+++ FlightComponents/Telemeter/ValveContoroler/ValveContoroler.ino
@@ -1,12 +1,10 @@
 #include "VarSpeedServo.h"
 #include <ArduinoJson.h>
 
+
+//Servo設定
 VarSpeedServo Mainservo;
 VarSpeedServo Supplyservo;
-
-// Servo Mainservo;
-// Servo Supplyservo;
-
 int MainServoPin = 5;
 int SupplyServoPin = 6;
 float Supplyservo_deg;
@@ -14,7 +12,7 @@ float Mainservo_deg;
 
 volatile int t = 0;
 
-StaticJsonDocument<32> ServoDeg;
+//StaticJsonDocument<32> ServoDeg;
 
 void setup()
 {
@@ -32,91 +30,59 @@ void setup()
 
 void loop()
 {
-    //serializeJson(ServoDeg, Serial);
-    //serializeJson(ServoDeg, Serial.write());
-
     if (t == 1)
     {
         t = 0;
         L_Position();
-        delay(10);
+        delay(300);
+        
+        /*
         ServoDeg.clear();
         ServoDeg["mainservoDeg"] = String(Mainservo_deg, 2);
         ServoDeg["supplyservoDeg"] = String(Supplyservo_deg, 2);
         serializeJson(ServoDeg, Serial);
+        */
     }
 
     if (t == 2)
     {
         t = 0;
         W_Position();
-        delay(10);
+        delay(300);
+        
+        /*
         ServoDeg.clear();
         ServoDeg["mainservoDeg"] = String(Mainservo_deg, 2);
         ServoDeg["supplyservoDeg"] = String(Supplyservo_deg, 2);
         serializeJson(ServoDeg, Serial);
+        */
     }
 }
 
 void L_SW()
 {
     t = 1;
-    // detachInterrupt(digitalPinToInterrupt(2));
 }
 
 void W_SW()
 {
     t = 2;
-    // detachInterrupt(digitalPinToInterrupt(3));
 }
 
 void L_Position()
 {
-    Supplyservo.write(90, 60, true);
-    // Supplyservo.write(90);
+    Supplyservo.write(90, 60, true); //SupplyServo CLOSE
     Supplyservo_deg = Supplyservo.read();
     delay(1000);
-
-    /*
-    for (int pos = 0; pos <= 90; pos += 1)
-    {
-        Mainservo.write(pos);
-        delay(20);
-        Mainservo_deg = Mainservo.read();
-    }
-    */
-
-    /*
-    if (Mainservo_deg == 90)
-    {
-        Mainservo.write(90);
-    }
-    */
-    Mainservo.write(140, 30, true);
+    Mainservo.write(140, 30, true); //MainServo OPEN
     Mainservo_deg = Mainservo.read();
 }
 
 void W_Position()
 {
-    Supplyservo.write(0, 60, true);
-    // Supplyservo.write(0);
+    Supplyservo.write(0, 60, true); //SupplyServo OPEN
     Supplyservo_deg = Supplyservo.read();
     delay(1000);
-
-    /*
-    for (int pos = 90; pos >= 0; pos -= 1)
-    {
-        Mainservo.write(pos);
-        delay(20);
-        Mainservo_deg = Mainservo.read();
-    }
-
-    if (Mainservo_deg == 0)
-    {
-        Mainservo.write(0);
-    }
-    */
-
-    Mainservo.write(0, 30, true);
+    Mainservo.write(0, 30, true); //MainServo CLOSE
     Mainservo_deg = Mainservo.read();
 }
