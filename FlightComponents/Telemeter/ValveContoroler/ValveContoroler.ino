a276d9acf4ce620fe9b42e843f0c14d53fb30b7f
Hiroki <hiroki1815@outlook.jp>
2022-12-10 02:02:28 +0900

    外部割込みの消去

--- FlightComponents/Telemeter/ValveContoroler/ValveContoroler.ino
+++ FlightComponents/Telemeter/ValveContoroler/ValveContoroler.ino
@@ -1,4 +1,89 @@
 #include "VarSpeedServo.h"
+
+constexpr int POSITION_CHANGING_THRESHOLD = 10;
+
+int LCount = 0;
+int WCount = 0;
+
+// 1: Waiting, 2: Launch
+int Position = 0;
+
+VarSpeedServo Mainservo;
+VarSpeedServo Supplyservo;
+int MainServoPin = 5;
+int SupplyServoPin = 6;
+
+void setup()
+{
+    Serial.begin(9600);
+    Mainservo.attach(MainServoPin);
+    Supplyservo.attach(SupplyServoPin);
+
+    pinMode(2, INPUT_PULLUP);
+    pinMode(3, INPUT_PULLUP);
+}
+
+void loop()
+{
+    // WaitingポジションかつLaunch信号がHIGHならLCountを加算する。それ以外ならLCountを0にリセットする
+    if (Position == 1 && digitalRead(2) == HIGH) {
+        LCount++;
+    } else {
+        LCount = 0;
+    }
+
+    // LCountが閾値以上になればLaunchポジションに変更する
+    if (LCount >= POSITION_CHANGING_THRESHOLD) {
+        LCount = 0;
+
+        L_Position();
+        Position = 2;
+        delay(300);
+    }
+
+    // 以下、WaitingとLaunchが逆になったバージョン
+
+    if (Position == 2 && digitalRead(3) == HIGH) {
+        WCount++;
+    } else {
+        WCount = 0;
+    }
+
+    if (WCount >= POSITION_CHANGING_THRESHOLD) {
+        WCount = 0;
+
+        W_Position();
+        Position = 1;
+        delay(300);
+    }
+
+    delay(10);
+}
+
+void L_Position()
+{
+    Supplyservo.write(90, 60, true); //SupplyServo CLOSE
+    delay(1000);
+    Mainservo.write(140, 30, true); //MainServo OPEN
+    Serial.println("LUNCH");
+}
+
+void W_Position()
+{
+    Supplyservo.write(0, 60, true); //SupplyServo OPEN
+    delay(1000);
+    Mainservo.write(0, 30, true); //MainServo CLOSE
+    Serial.println("WAITING");
+}
+
+
+
+
+
+
+
+/*
+#include "VarSpeedServo.h"
 #include <ArduinoJson.h>
 
 
@@ -39,12 +124,12 @@ void loop()
         L_Position();
         delay(300);
 
-        /*
+        
         ServoDeg.clear();
         ServoDeg["mainservoDeg"] = String(Mainservo_deg, 2);
         ServoDeg["supplyservoDeg"] = String(Supplyservo_deg, 2);
         serializeJson(ServoDeg, Serial);
-        */
+        
     }
 
     if (WCount => POSITION_CHANGING_THRESHOLD) {
@@ -53,12 +138,12 @@ void loop()
         W_Position();
         delay(300);
 
-        /*
+        
         ServoDeg.clear();
         ServoDeg["mainservoDeg"] = String(Mainservo_deg, 2);
         ServoDeg["supplyservoDeg"] = String(Supplyservo_deg, 2);
         serializeJson(ServoDeg, Serial);
-        */
+        
     }
 
     delay(10);
@@ -93,3 +178,5 @@ void W_Position()
     Mainservo.write(0, 30, true); //MainServo CLOSE
     Mainservo_deg = Mainservo.read();
 }
+*/
+
