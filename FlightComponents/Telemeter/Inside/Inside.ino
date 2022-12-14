e7b1ccc332b092484829746d841051cf02396254
TsutsumiHiroki <hiroki1815@outlook.jp>
2022-12-14 20:50:18 +0900

    deserializeJsonの追加

--- FlightComponents/Telemeter/Inside/Inside.ino
+++ FlightComponents/Telemeter/Inside/Inside.ino
@@ -12,17 +12,15 @@
 #include <Servo.h>
 #include <ArduinoJson.h>
 
-//サーボの設定
-//Servo mainservo;
-//Servo supplyservo;
+// サーボの設定
+// Servo mainservo;
+// Servo supplyservo;
 float supplyservo_deg;
 float mainservo_deg;
-//int supply_min_deg = 0;
-//int supply_max_deg = 90;
-// int main_min_deg = 0;
-// int main_max_deg = 140;
-
-volatile int t = 0;
+// int supply_min_deg = 0;
+// int supply_max_deg = 90;
+//  int main_min_deg = 0;
+//  int main_max_deg = 140;
 
 // GPSの設定
 float latitude;
@@ -34,6 +32,7 @@ unsigned long Time;
 
 // Arduinojsonの設定
 StaticJsonDocument<1024> downPacket_tlm;
+StaticJsonDocument<256> servoPacket;
 
 // LoRa addressの設定? <=2022/11/18時点では不明瞭
 byte localAddress = 0xBB; // address of this device
@@ -41,22 +40,9 @@ byte destination = 0xFF;  // where we are sending data to
 
 // millis()の設定
 unsigned long prev_SEND, interval_SEND;
-// unsigned long interval_SERVO, prev_SERVO;
-
-//割り込みの設定
-// int interruptPin_1 = 6;
-// int interruptPin_2 = 8;
 
 void setup()
 {
-
-    //スイッチピンの設定
-    pinMode(6, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(6), L_SW, RISING);
-
-    pinMode(8, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(8), W_SW, RISING);
-
     // initialize serial communications and wait for port to open:
     Serial.begin(9600);
 
@@ -74,21 +60,18 @@ void setup()
             ;
     }
 
-    // PWM出力ピンの設定
-    //mainservo.attach(5);
-    //supplyservo.attach(4);
-
     prev_SEND = 0;
     interval_SEND = 1000;
 
-    //　状態確認用LED
-    // PinMode(PIN, OUTPUT);
-    // PinMode(PIN, OUTPUT);
+    // 　状態確認用LED
+    //  PinMode(PIN, OUTPUT);
+    //  PinMode(PIN, OUTPUT);
 }
 
 void loop()
 {
     // check if there is new GPS data available
+    /*
     if (GPS.available())
     {
         // read GPS values
@@ -101,134 +84,27 @@ void loop()
 
         // Create and send LoRa packet
         downlinkFlightData_tlm();
-    }
+    }*/
+    latitude = GPS.latitude();
+    longitude = GPS.longitude();
+    altitude = GPS.altitude();
+    speed = GPS.speed();
+    satellites = GPS.satellites();
+    Time = GPS.getTime();
 
-    if (t == 1)
-    {
-        t = 0;
-        mainservo_deg = 140;
-        supplyservo_deg = 90;
-        // digitalWrite(PIN, HIGH); //OPEN LED
-        // digitalWrite(PIN, LOW);  //CLOSE LED
-    }
+    downlinkFlightData_tlm();
 
-    if (t == 2)
-    {
-        t = 0;
-        mainservo_deg = 0;
-        supplyservo_deg = 0;
-        // digitalWrite(PIN, LOW);  //OPEN LED
-        // digitalWrite(PIN, HIGH); //CLOSE LED
-    }
+    deserializeJson(servoPacket, Serial1);
+    mainservo_deg = servoPacket["mainservoDeg"];
+    supplyservo_deg = servoPacket["supplyservoDeg"];
 }
-
-/*
-void LUNCH_Position()
-{
-    supplyservo.write(supply_max_deg);
-    supplyservo_deg = supplyservo.read();
-    delay(1000);
-    for (pos = 0; pos >= 120; pos += 1)
-    {
-        mainservo.write(pos);
-        delayMicroseconds(500000);
-        mainservo_deg = mainservo.read();
-    }
-
-
-    if (mainservo_deg == 120)
-    {
-        // mainservo.detach(5);
-    }
-
-}
-*/
-
-/*
-void WAITING_Position()
-{
-    supplyservo.write(supply_min_deg);
-    supplyservo_deg = supplyservo.read();
-    delay(1000);
-    for (pos = 120; pos <= 0; pos -= 1)
-    {
-        mainservo.write(pos);
-        delayMicroseconds(500000);
-        mainservo_deg = mainservo.read();
-    }
-
-
-    if (mainservo_deg == 0)
-    {
-        // mainservo.detach(5);
-    }
-}
-*/
-
-/*
-void LUNCH_Position()
-{
-    supplyservo.write(supply_max_deg);    // CLOSE
-    supplyservo_deg = supplyservo.read(); //現在の角度を取得
-    Serial.println(supplyservo.read());
-
-    //供給路と主流路が同時に開いている状態の防止
-    // delayMicroseconds(1000000); // 1,000,000us = 1,000,000E-6s = 1s
-
-    if (supplyservo_deg == 60)
-    {
-        mainservo.write(main_max_deg);    // OPEN
-        mainservo_deg = mainservo.read(); //現在の角度を取得
-        Serial.println(mainservo.read());
-        // digitalWrite(3, HIGH);
-        // digitalWrite(Pin, LOW);
-        Serial.println("-- Complete Lunch Position --");
-        Serial.println();
-    }
-}
-*/
-
-/*
-void WAITING_Position()
-{
-    supplyservo.write(supply_min_deg);    // OPEN
-    supplyservo_deg = supplyservo.read(); //現在の角度を取得
-    Serial.println(supplyservo.read());
-
-    //供給路と主流路が同時に開いている状態の防止
-    // delayMicroseconds(1000000); // 1,000,000us = 1,000,000E-6s = 1s
-    for ()
-
-    if (supplyservo_deg == 0)
-    {
-        mainservo.write(main_min_deg);    // CLOSE
-        mainservo_deg = mainservo.read(); //現在の角度を取得
-        Serial.println(mainservo.read());
-        // digitalWrite(3, LOW);
-        // digitalWrite(PIN, HIGH);
-        Serial.println("== Complete WAITING Position ==");
-        Serial.println();
-    }
-}
-*/
-
-void L_SW()
-{
-    t = 1;
-}
-
-void W_SW()
-{
-    t = 2;
-}
-
-// Safety ArmedからSafeにした際にサーボが駆動するが、これはサーボが0度以外になっている場合に0に戻っている（つまりダンプ可能な状態）
-//同時に割り込みが入った場合は、上から順に読まれていくので、LUNCHI_Position => WAITING_Positionの順となる
 
 void downlinkFlightData_tlm()
 {
+    /*
     if (!LoRa.begin(923E6))
         return;
+*/
 
     unsigned long curr_SEND = millis();
     if ((curr_SEND - prev_SEND) >= interval_SEND)
