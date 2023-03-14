87fed003574f1d3a1914c2ebf2971dd9bbcbc279
TsutsumiHiroki <hiroki1815@outlook.jp>
2023-03-15 00:36:51 +0900

    コメントを更新

--- FlightComponents/Telemeter/ValveContoroler/ValveContoroler.ino
+++ FlightComponents/Telemeter/ValveContoroler/ValveContoroler.ino
@@ -1,15 +1,14 @@
 /*
-  Writer?:Hiroki TSUTSUMI(1CES2116)
-  Rocket:H-57
-  MCU Series:Arduino
-  MCU:SAMD21
+  Auther    Hiroki TSUTSUMI(1CES2116)
+  Rocket    H-57
+  MCU       Series:Arduino
+  MCU       ATMEGA328P
 
   バルブ位置を制御し、テレメトリーモジュールに送信する機能を持っています。
 */
 
 #include "VarSpeedServo.h"
 #include <ArduinoJson.h>
-
 
 constexpr int POSITION_CHANGING_THRESHOLD = 10;
 
@@ -44,17 +43,16 @@ void setup()
 
     pinMode(2, INPUT_PULLUP); // WAITING <=- Arduino UNO　//LAUNCH <=- Arduino NANO　//ただの設計ミス。ソフトウェアで解決可能なのでUNO,NANOで切り替える
     pinMode(3, INPUT_PULLUP); // LUNCH <=- ArduinoUNO //WAITING <=- Arduino NANO
-    
-    //pinMode(3, INPUT_PULLUP); // WAITING <=- Arduino UNO　//LAUNCH <=- Arduino NANO　//ただの設計ミス。ソフトウェアで解決可能なのでUNO,NANOで切り替える
-    //pinMode(2, INPUT_PULLUP); // LUNCH <=- ArduinoUNO //WAITING <=- Arduino NANO
-    
 
-    //pinMode(LED_BUILTIN, OUTPUT); //UNO
-    pinMode(5, OUTPUT); //NANO
-    pinMode(6, OUTPUT); //NANO
+    // pinMode(3, INPUT_PULLUP); // WAITING <=- Arduino UNO　//LAUNCH <=- Arduino NANO　//ただの設計ミス。ソフトウェアで解決可能なのでUNO,NANOで切り替える
+    // pinMode(2, INPUT_PULLUP); // LUNCH <=- ArduinoUNO //WAITING <=- Arduino NANO
 
-    digitalWrite(6, HIGH); //NANO
-    digitalWrite(5, LOW); //NANO
+    // pinMode(LED_BUILTIN, OUTPUT); //UNO
+    pinMode(5, OUTPUT); // NANO
+    pinMode(6, OUTPUT); // NANO
+
+    digitalWrite(6, HIGH); // NANO
+    digitalWrite(5, LOW);  // NANO
 
     delay(1000);
 }
@@ -62,8 +60,8 @@ void setup()
 void loop()
 {
     // WaitingポジションかつLaunch信号がHIGHならLCountを加算する。それ以外ならLCountを0にリセットする
-    //if (Position == 1 && digitalRead(2) == LOW) //NANO
-    if (Position == 1 && digitalRead(3) == LOW) //UNO
+    // if (Position == 1 && digitalRead(2) == LOW) //NANO
+    if (Position == 1 && digitalRead(3) == LOW) // UNO
     {
         LCount++;
     }
@@ -83,8 +81,8 @@ void loop()
     }
 
     // 以下、WaitingとLaunchが逆になったバージョン
-    //if (Position == 2 && digitalRead(3) == LOW) //NANO
-    if (Position == 2 && digitalRead(2) == LOW) //UNO
+    // if (Position == 2 && digitalRead(3) == LOW) //NANO
+    if (Position == 2 && digitalRead(2) == LOW) // UNO
     {
         WCount++;
     }
@@ -115,63 +113,73 @@ void loop()
     //  Serial.print(",");
     //  Serial.println(LCount);
 
-    //delayMicroseconds(100);
+    // delayMicroseconds(100);
     delay(2); // 250Hz
-    //delay(10); // 100Hz
+    // delay(10); // 100Hz
 }
 void L_Position()
 {
-    Supplyservo.write(80, 60, true); // SupplyServo CLOSE //0がMAX Speed Fill弁が閉まる時間:???s
-    //FILLバルブ：80度で閉
-    //delay(200);
     // 2023/1/21 フローテック -=> Sweagelock にバルブ変更
-    Mainservo.write(57, 50, true); // UNO //NANO
+    // SERVO.write(deg, speed, true)
+    Supplyservo.write(80, 60, true);        // UNO, NANO // Supply Valve CLOSE //0:MAX Speed 1-255:Slower-Faster
+    // FILLバルブ：80度で閉
 
-    // digitalWrite(LED_BUILTIN, HIGH);
-    digitalWrite(5, HIGH); //NANO
-    digitalWrite(6, LOW); //NANO
+    delay(200);                             //処理落ち防止用
 
-    Supplyservo_deg = Supplyservo.read();
-    Mainservo_deg = Mainservo.read();
-    // 2022/12/21
-    // コールドフロー試験
-    // Mainservo.write(140, 30, true); 角度140°、速度30にて成功
-    // 成功を踏まえて...速度を早くするべきかどうか検討する必要あり。
+    // SERVO.write(deg, speed, true)
+    Mainservo.write(57, 50, true);          // UNO, NANO // Main Valve OPEN //0:MAX Speed 1-255:Slower-Faster
+
+    // digitalWrite(LED_BUILTIN, HIGH);     // UNO //状態確認用
+    digitalWrite(5, HIGH);                  // NANO //Valve OPEN LED HIGH
+    digitalWrite(6, LOW);                   // NANO //Valve CLOSE LED LOW
+
+    Supplyservo_deg = Supplyservo.read();   //Supple Servo 角度の読み出し
+    Mainservo_deg = Mainservo.read();       //Main Serovo 角度読み出し
+
+    // メモ BEGIN
+    //  2022/12/21
+    //  コールドフロー試験
+    //  Mainservo.write(140, 30, true); 角度140°、速度30にて成功
+    //  成功を踏まえて...速度を早くするべきかどうか検討する必要あり。
     //
-    // 2022/12/23
-    // 燃焼実験
-    // Mainservo.write(140,50, true);角度140°、速度50で実験
-    // 点火成功！
+    //  2022/12/23
+    //  燃焼実験
+    //  Mainservo.write(140,50, true);角度140°、速度50で実験
+    //  点火成功！
     //
-    // 2023/1/11
-    // コールドフロー試験
-    // Supplyservo.write(60, 60, true);
-    // Supplyservo_deg = Supplyservo.read();
-    // delay(10);
-    // Mainservo.write(140, 50, true);
-    // Mainservo_deg = Mainservo.read();
-    // digitalWrite(LED_BUILTIN, HIGH);
-    // 上記の動作でコールドフロー試験を成功させることが出来た。やったね！
+    //  2023/1/11
+    //  コールドフロー試験
+    //  Supplyservo.write(60, 60, true);
+    //  Supplyservo_deg = Supplyservo.read();
+    //  delay(10);
+    //  Mainservo.write(140, 50, true);
+    //  Mainservo_deg = Mainservo.read();
+    //  digitalWrite(LED_BUILTIN, HIGH);
+    //  上記の動作でコールドフロー試験を成功させることが出来た。やったね！
     //
-    // Swagelockバルブでのコールドフロー試験
-    // Supplyservo.write(80, 255, true); // SupplyServo CLOSE //0:MAX Speed 1-255:Slower-Faster
-    // delay(100);
-    // Mainservo.write(57, 40, true); // UNO //NANO
+    //  Swagelockバルブでのコールドフロー試験
+    //  Supplyservo.write(80, 255, true); // SupplyServo CLOSE
+    //  delay(100);
+    //  Mainservo.write(57, 40, true); // UNO //NANO
+    // メモ END
 }
 
 void W_Position()
 {
-    
-    Supplyservo.write(20, 30, true); // SupplyServo OPEN
-    delay(10);
-    Mainservo.write(20, 30, true); // MainServo CLOSE
-    
+    // SERVO.write(deg, speed, true)
+    Supplyservo.write(20, 30, true);        // Supply Valve OPEN
 
-    // digitalWrite(LED_BUILTIN, LOW); //UNO
-    digitalWrite(6, HIGH); //NANO
-    digitalWrite(5, LOW); //NANO
-    Supplyservo_deg = Supplyservo.read();
-    Mainservo_deg = Mainservo.read();
+    delay(10);                              //処理落ち防止用　
+    
+    // SERVO.write(deg, speed, true)
+    Mainservo.write(20, 30, true);          // Main Valve CLOSE
+
+    // digitalWrite(LED_BUILTIN, LOW);      // UNO //状態確認用
+    digitalWrite(5, LOW);                   // NANO //Valve OPEN LED HIGH
+    digitalWrite(6, HIGH);                  // NANO //Valve CLOSE LED LOW
+
+    Supplyservo_deg = Supplyservo.read();   //Supple Servo 角度の読み出し
+    Mainservo_deg = Mainservo.read();       //Main Servo 角度読み出し
 }
 
 /*
